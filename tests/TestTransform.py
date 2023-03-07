1530fe16b0d580ed1f9e22d644d3d1163c16db88
Diego Acosta <dacostaortiz@outlook.com>
2023-03-07 00:26:15 -0500

    Tests for basic transforming functions for each table to migrate

---
+++ tests/TestTransform.py
@@ -0,0 +1,44 @@
+import unittest
+from app.etl.transform.Transform import Transform
+
+class TestTransform(unittest.TestCase):
+
+    def test_transform_hired_employees(self):
+        input_data = [
+            ['4535', 'Marcelo Gonzalez', '2021-07-27T16:02:08Z', '1', '2'],
+            ['4572', 'Lidia Mendez', '2021-07-27T19:04:09Z', '1', '2']
+        ]
+        expected_data = [
+            {'id': 4535, 'name': 'Marcelo Gonzalez', 'datetime': '2021-07-27T16:02:08Z', 'department_id': 1, 'job_id': 2},
+            {'id': 4572, 'name': 'Lidia Mendez', 'datetime': '2021-07-27T19:04:09Z', 'department_id': 1, 'job_id': 2}
+        ]
+        actual_data = Transform.transform_hired_employees(input_data)
+        self.assertEquals(actual_data,expected_data)
+
+    def test_transform_departments(self):
+        input_data = [
+            ['1', 'Supply Chain'],
+            ['2', 'Maintenance'],
+            ['3', 'Staff']
+        ]
+        expected_data = [
+            {'id': 1, 'department': 'Supply Chain'},
+            {'id': 2, 'department': 'Maintenance'},
+            {'id': 3, 'department': 'Staff'}
+        ]
+        actual_data = Transform.transform_departments(input_data)
+        self.assertEquals(actual_data, expected_data)
+
+    def test_transform_jobs(self):
+        input_data = [
+            ['1', 'Recruiter'],
+            ['2', 'Manager'],
+            ['3', 'Analyst']
+        ]
+        expected_data = [
+            {'id': 1, 'job': 'Recruiter'},
+            {'id': 2, 'job': 'Manager'},
+            {'id': 3, 'job': 'Analyst'}
+        ]
+        actual_data = Transform.transform_jobs(input_data)
+        self.assertEquals(actual_data, expected_data)
